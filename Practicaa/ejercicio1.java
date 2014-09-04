58c6059d60ca15028758004865c75cf2acb17c74
ingri_000 <ingri_000@Equipo_Paola>
2014-09-04 15:50:33 -0500

    primer archivos subidos

---
+++ Practicaa/ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ingri_000
+ */
+public class ejercicio1 {
+    
+public static void main(String[] args){
+    System.out.println("esta es una prueba");
+}    
+    
+}
